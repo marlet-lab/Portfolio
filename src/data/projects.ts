c7a01e546cea2b85ea64aacdea5983aaeb4f2009
Maria Leth <marialeth01@gmail.com>
2026-09-04 21:15:48 +0200

    added project component

---
+++ src/data/projects.ts
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        title: "Prototyping and Research (report)",
+        description: "Mobile App Design",
+        image: "/images/project-1.jpg",
+        href: "/projects/prototyping-research"
+    },
+
+    {
+        title: "Collaboration with an Australian marketing team at USM",
+        description: "React, javascript, firebase, API",
+        image: "/images/project-2.jpg",
+        href: "/projects/usm"
+    },
+
+    {
+        title: "HERO Imaging",
+        description: "User testing and developing their node system",
+        image: "/images/project-3.jpg",
+        href: "/projects/hero-imaging"
+    },
+
+    {
+        title: "EPL",
+        description: "React EXPO, Figma",
+        image: "/images/project-4.jpg",
+        href: "/projects/epl"
+    }
+];
