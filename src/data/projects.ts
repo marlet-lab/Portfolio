4d5d7f754eaa6b5e6564ee37763478e2237cf099
Maria Leth <marialeth01@gmail.com>
2026-09-12 15:17:23 +0200

    added carousel to aobut and started project description page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,27 +3,27 @@ export const projects = [
         title: "Prototyping and Research (report)",
         description: "Mobile App Design",
         image: "/images/project-1.jpg",
-        href: "/projects/prototyping-research"
+        href: "/projects/project-one"
     },
 
     {
         title: "Collaboration with an Australian marketing team at USM",
         description: "React, javascript, firebase, API",
         image: "/images/project-2.jpg",
-        href: "/projects/usm"
+        href: "/projects/project-two"
     },
 
     {
         title: "HERO Imaging",
         description: "User testing and developing their node system",
         image: "/images/project-3.jpg",
-        href: "/projects/hero-imaging"
+        href: "/projects/project-three"
     },
 
     {
         title: "EPL",
         description: "React EXPO, Figma",
         image: "/images/project-4.jpg",
-        href: "/projects/epl"
+        href: "/projects/project-four"
     }
 ];
